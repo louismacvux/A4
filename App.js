36b4b099f34f8a01a27ece027930ed9c6fb295d9
louismacvux <57550224+louismacvux@users.noreply.github.com>
2024-12-18 23:56:10 -0600

    add basic profile

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { Asset } from "expo-asset";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.profile}>
+          <Image
+            source={Asset.fromModule(require("./assets/bussiness-man.png"))}
+            style={styles.image}
+          ></Image>
+          <Text style={styles.header}>Minh Long Vu</Text>
+        </View>
+        <View style={styles.section}>
+          <Text>Contact Information</Text>
+          <Text>
+            <Text style={{ fontWeight: "bold" }}>Email: </Text>
+            <Text>vu-m66@webmail.uwinnipeg.ca</Text>
+          </Text>
+          <Text>
+            <Text style={{ fontWeight: "bold" }}>Phone: </Text>
+            <Text>(123) 456 7890</Text>
+          </Text>
+        </View>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    padding: 10,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 15,
+  },
+  profile: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  image: {
+    height: 70,
+    width: 70,
+    marginBottom: 15,
+  },
+  section: {
+    alignSelf: "flex-start",
   },
 });
